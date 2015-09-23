80f5976f59bd7e5affa7139cca56ef6aa9b282d7
Daan Boer <daanboer95@hotmail.com>
2015-09-23 10:11:26 +0200

    Ass. 4 part 1

---
+++ DrawingWithPython/DrawingWithPython/DrawingWithPython.py
@@ -0,0 +1,9 @@
+﻿import sys
+
+for x in range(0, 1000):
+    for z in range(x/2, 10):
+        sys.stdout.write(" ")
+    for y in range(0, x):
+        sys.stdout.write("*")
+    sys.stdout.write("\n")
+    x = x - 1
