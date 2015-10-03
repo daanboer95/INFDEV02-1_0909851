69e09d39dc1670932e12ccdd7d575d1851f550d2
Daan Boer <daanboer95@hotmail.com>
2015-10-03 18:44:45 +0200

    figures

---
+++ Ass4-2/Ass4-2/Ass4_2.py
@@ -0,0 +1,2 @@
+﻿d1 = raw_input("Player 1: Rock, Paper, Scizzers, Lizzard, Spock?\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n->")
+d2 = raw_input("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nPlayer 2: Rock, Paper, Scizzers, Lizzard, Spock?\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n->")
--- DrawingWithPython/DrawingWithPython/DrawingWithPython.py
+++ DrawingWithPython/DrawingWithPython/DrawingWithPython.py
@@ -1,9 +1,77 @@
 ﻿import sys
 
-for x in range(0, 1000):
+for x in range(0, 10):
     for z in range(x/2, 10):
         sys.stdout.write(" ")
     for y in range(0, x):
         sys.stdout.write("*")
     sys.stdout.write("\n")
     x = x - 1
+
+sys.stdout.write("\n\n*****************\n\n")
+
+
+for o in range(0, 10):
+    sys.stdout.write("*")
+sys.stdout.write("\n")
+for p in range(0, 8):
+    sys.stdout.write("*")
+    for k in range(0, 8):
+        sys.stdout.write(" ")
+    sys.stdout.write("*")
+    sys.stdout.write("\n")
+for o in range(0, 10):
+    sys.stdout.write("*")
+
+sys.stdout.write("\n\n*****************\n\n")
+
+for x in range(-5, 6):
+    for z in range(-abs(x), 1):
+        sys.stdout.write("*")
+    for y in range(0, ((5-abs(x))*2)):
+        sys.stdout.write(" ")
+    for z in range(-abs(x), 1):
+        sys.stdout.write("*")
+    sys.stdout.write("\n")
+
+sys.stdout.write("\n\n*****************\n\n")
+
+for x in range(-5, 6):
+    for z in range(-abs(x), 0):
+        sys.stdout.write(" ")
+    sys.stdout.write("*")
+    for y in range(0, ((5-abs(x))*2)):
+        sys.stdout.write(" ")
+    sys.stdout.write("*")
+    sys.stdout.write("\n")
+
+sys.stdout.write("\n\n*****************\n\n")
+
+for x in range(-3, 4):
+    if(x>-2 and x<2):
+        sys.stdout.write("*")
+    else:
+        sys.stdout.write(" ")
+    for y in range(0, int(2 ** (abs(x)^2 ))):
+        sys.stdout.write(" ")
+    sys.stdout.write("*")
+    sys.stdout.write("\n")
+
+sys.stdout.write("\n\n*****************\n\n")
+
+for x in range(-3, 4):
+    for y in range(3, (2 ** abs(x))):
+        sys.stdout.write(" ")
+    sys.stdout.write("*")
+    for y in range(3-(2 ** abs(x)), 3):
+        sys.stdout.write(" ")
+    sys.stdout.write("*")
+    sys.stdout.write("\n")
+
+sys.stdout.write("\n\n*****************\n\n")
+
+for x in range(-3, 4):
+    for y in range(0, (2 ** abs(x))):
+        sys.stdout.write(" ")
+    sys.stdout.write("*")
+    sys.stdout.write("\n")
