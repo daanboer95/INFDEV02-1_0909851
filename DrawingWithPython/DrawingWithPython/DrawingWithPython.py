af8bc686a0376230e039925ba658deb330e221a6
Daan Boer <daanboer95@hotmail.com>
2015-10-04 17:39:43 +0200

    figures

--- DrawingWithPython/DrawingWithPython/DrawingWithPython.py
+++ DrawingWithPython/DrawingWithPython/DrawingWithPython.py
@@ -59,19 +59,47 @@ for x in range(-3, 4):
 
 sys.stdout.write("\n\n*****************\n\n")
 
+
 for x in range(-3, 4):
+    a = 0
     for y in range(3, (2 ** abs(x))):
         sys.stdout.write(" ")
+        a = a + 1
     sys.stdout.write("*")
-    for y in range(3-(2 ** abs(x)), 3):
+    for z in range(0, (abs((5 - a)*2))):
         sys.stdout.write(" ")
     sys.stdout.write("*")
     sys.stdout.write("\n")
 
 sys.stdout.write("\n\n*****************\n\n")
 
-for x in range(-3, 4):
-    for y in range(0, (2 ** abs(x))):
+nr = 4
+
+for x in range((- nr - 1), nr):
+    a = 0
+    for y in range((nr - 1), (2 ** abs(x))):
+        sys.stdout.write(" ")
+        a = a + 1
+    sys.stdout.write("*")
+    for z in range(0, (abs(((nr + 1) - a)*2))):
         sys.stdout.write(" ")
     sys.stdout.write("*")
     sys.stdout.write("\n")
+
+sys.stdout.write("\n\n*****************\n\n")
+
+a = 14
+
+for x in range(0, (2*a)):
+    sys.stdout.write("*")
+for x in range(0, (a - 2)):
+    sys.stdout.write("\n")
+    sys.stdout.write("*")
+    for y in range(0, (2*a - 2)):
+        sys.stdout.write(" ")
+    sys.stdout.write("*")
+sys.stdout.write("\n")
+for x in range(0, (2*a)):
+    sys.stdout.write("*")
+
+sys.stdout.write("\n\n*****************\n\n")
